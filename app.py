9b19f87f0ea45e240fc95b0460ff34ece56e75ce
kamr8431 <rishikamath09@gmail.com>
2026-06-01 10:17:50 -0700

    Update app.run to use environment PORT variable

--- app.py
+++ app.py
@@ -160,6 +160,7 @@ def stats_psychological():
     }
     return render_template("stats/psychological.html", title=study['title'], analysis=study['analysis'])
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
